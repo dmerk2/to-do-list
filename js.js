098d8fa885a80cb43b409d5c8e5ffbd9d3ef84a4
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-08-30 20:47:31 -0400

    added css and button

--- js.js
+++ js.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 	$('#btn').click(function(){
 	  var add = $('#input').val();
-		$('ol').append("<li>" + add + '</li>');
+		$('ol').append("<li>" + add + '</li>' + add + "<button>" + add + "<i>");
+		$("delete").val()
 	});
 });
 
+
+	//HOW TO MAKE BUTTON CLEAR??????????
+	// $('#clearButton').click(function(){
+	// 	$('#todoList').empty()
+	// 	alert("clear")
+	// });
