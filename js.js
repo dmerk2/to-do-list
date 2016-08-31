4600629fb3ba2247b66796ed2dd83bc663c2c7f6
Daniel Merkin <danmerkin@Dans-MBP.home>
2016-08-31 03:59:09 -0400

    deleted clone and worked on buttons

--- js.js
+++ js.js
@@ -6,10 +6,12 @@ $(document).ready(function(){
 	});
 });
 
+// HOW TO MAKE BUTTON CLEAR??????????
+// 	$('.clearButton').click(function() {
+// 	$("input[type='text']").empty().val();	
+// };
 
-
-	//HOW TO MAKE BUTTON CLEAR??????????
-	// $('#clearButton').click(function(){
-	// 	$('#todoList').empty()
-	// 	alert("clear")
-	// });
+//HOW DO I REMOVE THE ONE BUTTON BEFORE ANY LI'S AND DELETE BUTTONS SINGULARLY???
+//HOW DO I MAKE IT RESPONSIVE????
+//ASK AFTER BEING CHECKED IF SURE WANT IT DELETED WITH MODAL
+//HOW TO MOVE BUTTON AND MAKE COLUMNS/ROWS
